653a3095c740a100ffb88419e91c5cf8800c9ad6
Aatish <aatishda1@gmail.com>
2024-05-24 17:49:34 +0100

    Heroku Connection

--- synbiosource/synbiosource/settings.py
+++ synbiosource/synbiosource/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["synbiosource-04151ad4f184.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
